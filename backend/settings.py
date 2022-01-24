343803e257aa0a7db28ce29bf497d25fb5068250
joel <jobaff03@gmail.com>
2022-01-24 14:00:47 +0000

    staus

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ze0!cq%q_e+2w77vuy&g7t313+f6y8_)5kq3k3v51nr8e5ajdx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['wtestbuild2.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
